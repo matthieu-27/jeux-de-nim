2aa31bb33d348d0fe108a0a69a550d094cc57b98
matthieu-27 <matthieu27@pm.me>
2025-10-13 14:51:06 +0200

    Added docstrings

--- nim_games.py
+++ nim_games.py
@@ -14,16 +14,30 @@ MAX = 5
 
 
 def get_player(turn_number):
+    """
+    Returns player string relative to `turn_number`
+    :param turn_number: The turn number integer
+    :return: The player nickname
+    """
     return first_player_str if turn_number % 2 == 0 else second_player_str
 
 
 def check_matches(match_number, player_number):
+    """
+    Game logic for Nim game
+    :param match_number: number of matches
+    :param player_number: choosen player number
+    :return: True if we can take matches, False if matches at 0
+    """
     if match_number - player_number >= 1:
         return True
     return False
 
 
 def display_humans(match_number, turn_number):
+    """
+    Display a game in a Human versus Human format
+    """
     int_str = int(input(f"Tour {turn_number}: Combien d'allumettes voulez vous {get_player(turn_number)}?\n"))
     if int_str in range(MIN, MAX):
         if check_matches(match_number, int_str):
@@ -38,6 +52,9 @@ def display_humans(match_number, turn_number):
 
 
 def display_computer(match_number, turn_number):
+    """
+    Display a game in a Human versus Computer format
+    """
     global last_one
     if get_player(turn_number) == first_player_str:
         int_str = int(input(f"Tour {turn_number}: Combien d'allumettes voulez vous {first_player_str}?\n"))
