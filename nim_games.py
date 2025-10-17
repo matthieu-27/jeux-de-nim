723cf5d95e5a3184105704eb847dcaa57501b653
matthieu-27 <matthieu27@pm.me>
2025-10-17 15:29:51 +0200

    Added Marienbad variation

--- nim_games.py
+++ nim_games.py
@@ -33,7 +33,7 @@ def check_matches(_match_number, _player_number):
 def check_matches_marienbad(_marienbad, _player_number, _pile_number):
     """
     Game logic for Nim game (Marienbad)
-    :param _match_number: number of matches
+    :param _marienbad: number of matches (Marienbad)
     :param _player_number: choosen player number
     :return: True if we can take matches, False if matches at 0
     """
@@ -42,40 +42,40 @@ def check_matches_marienbad(_marienbad, _player_number, _pile_number):
     return False
 
 
-def display_humans(_marienbad, _turn_number):
+def display_humans(match_number, turn_number):
     """
     Display a game in a Human versus Human format
     """
-    int_str = int(input(f"Tour {_turn_number}: Combien d'allumettes voulez vous {get_player(_turn_number)}?\n"))
+    int_str = int(input(f"Tour {turn_number}: Combien d'allumettes voulez vous {get_player(turn_number)}?\n"))
     if int_str in range(MIN, MAX):
-        if check_matches_marienbad(_marienbad, int_str):
-            _marienbad -= int_str
-            _turn_number += 1
+        if check_matches(match_number, int_str):
+            match_number -= int_str
+            turn_number += 1
         else:
-            print(f"Il reste{_marienbad} allumettes. C'est terminé {get_player(_turn_number)}")
+            print(f"Il reste {match_number} allumettes. C'est terminé {get_player(turn_number)}")
             exit()
     else:
         print(f"Entrez un chiffre entre {MIN} et {MAX} svp")
-    display_humans_marienbad(_marienbad, _turn_number)
+    display_humans(match_number, turn_number)
 
 
 def display_humans_marienbad(_marienbad, _turn_number):
     """
-    Display a game in a Human versus Human format
+    Display a game in a Human versus Human format (Marienbad)
     """
-    pile_str = int(input(f"Quel tas choisissez vous ?"))
+    pile_str = int(input(f"Quel tas choisissez vous {get_player(_turn_number)}?"))
     int_str = int(input(f"Tour {_turn_number}: Combien d'allumettes voulez vous ?\n"))
-    _turn_number += 1
     if int_str in range(MIN, MAX):
         if check_matches_marienbad(_marienbad, int_str, pile_str):
-            var = _marienbad[pile_str] - int_str
+            _marienbad[pile_str] = _marienbad[pile_str] - int_str
             _turn_number += 1
         else:
-            print(f"Il reste{_marienbad[pile_str]} allumettes. C'est terminé {get_player(_turn_number)}")
+            print(f"Il reste {_marienbad[pile_str]} allumettes. C'est terminé {get_player(_turn_number)}")
             exit()
     else:
         print(f"Entrez un chiffre entre {MIN} et {MAX} svp")
-    display_humans_marienbad(_marienbad, _turn_number)
+    _turn_number += 1
+    display_marienbad(_marienbad, _turn_number)
 
 
 def computer_5_minus_n(_played_number, _turn_number):
@@ -85,6 +85,16 @@ def computer_5_minus_n(_played_number, _turn_number):
     computer_take = 5 - _played_number
     _turn_number += 1
     return computer_take, _turn_number
+
+
+def display_marienbad(_marienbad, _turn_number, _count):
+    _str = ''
+    for n in marienbad:
+        _str += f" |  - Tas numéro {_marienbad.index(n) + 1}" if n == len(marienbad) else " | "
+    print(f"Tour numéro {_turn_number}\n" if count == 1 else "")
+    _count += 1
+    print(_str)
+
 
 
 def compute_marienbad(_marienbad, _played_number, _turn_number):
@@ -156,7 +166,7 @@ def is_legal_move_marienbad(_marienbad, _played_number, _pile_number, _turn_numb
 if __name__ == '__main__':
     player_one, player_two = str(input("Nom du joueur 1 et 2 (séparés par ,) ?\n")).split(',')
     game_mode = int(input("Mode de jeu: 1 - Humain vs Humain | 2 - Humain vs ordinateur | "
-                          "3 - Humain vs Humain (Marienbad) | 4 - Humain vs ordinateur (Marienbad)\n"))
+                          "3 - Humain vs ordinateur (Marienbad) | 4 - Humain vs humain (Marienbad)\n"))
     turn, last_pick = 0, 0
     game_over = False
     match_number = MAX_MATCHES
@@ -166,7 +176,7 @@ if __name__ == '__main__':
         if not game_over and turn == 0:
             match_number = 21
         if game_mode == 1:
-            display_humans_marienbad(match_number, turn)
+            display_humans(match_number, turn)
         if game_mode == 2:
             if turn % 2 == 0:
                 last_pick, turn = ask_player_input(turn)
@@ -178,21 +188,22 @@ if __name__ == '__main__':
                 if not is_legal_move(match_number, turn, computer_number):
                     game_over = True
                 match_number -= computer_number
-        if game_mode == 3:
-            display_humans_marienbad(marienbad, turn)
         if game_mode == 4:
             count = 1
+            display_marienbad(marienbad, turn, count)
+        if game_mode == 3:
+            count = 1
             for match in marienbad:
-                _str = ""
+                str = ""
                 for num in range(match):
-                    _str += f" | -- Tas numéro {count}" if num + 1 == len(range(match)) else " | "
+                    str += f" | -- Tas numéro {count}" if num + 1 == len(range(match)) else " | "
                 count += 1
-                print(f"{_str}")
+                print(f"{str}")
             if turn % 2 == 0:
                 last_pick, pile, turn = ask_player_input_marienbad(turn)
                 if not is_legal_move_marienbad(marienbad, last_pick, pile, turn):
                     game_over = True
-                var = marienbad[pile] - last_pick
+                marienbad[pile] = marienbad[pile] - last_pick
             else:
                 computer_pick, pile, turn = compute_marienbad(last_pick, turn)
                 if not is_legal_move_marienbad(marienbad, computer_pick, pile, turn):
